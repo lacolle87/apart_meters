6dd69fd3f0548c3f666d8b8e0c9c835aa58d4473
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-12 01:03:44 +0300

    write more models

--- database/models.py
+++ database/models.py
@@ -1,30 +1,44 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     chat_id = Column(Integer, unique=True, nullable=False)
-    username = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True)
     first_name = Column(String)
     water_metrics = relationship('Metric', back_populates='user')
+    apartment_id = Column(Integer, ForeignKey('appartments.id'))  # Corrected foreign key
+    live = Column(Boolean)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 
 class Rates(Base):
     __tablename__ = 'rates'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     electric_rate = Column(Float)
     water_rate = Column(Float)
     drainage_rate = Column(Float)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Appartments(Base):
+    __tablename__ = 'appartments'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 
 class Metric(Base):
     __tablename__ = 'metrics'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     water_usage = Column(Float)
     electric_usage = Column(Float)
     user = relationship('User', back_populates='metrics')
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
