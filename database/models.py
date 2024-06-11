42aec9329f9f51902c9b8a9b4b4f6d7b2d63c223
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-12 01:22:22 +0300

    add TimestampMixin to models

--- database/models.py
+++ database/models.py
@@ -4,38 +4,36 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class User(Base):
+class TimestampMixin:
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class User(Base, TimestampMixin):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     chat_id = Column(Integer, unique=True, nullable=False)
     username = Column(String, unique=True)
     first_name = Column(String)
-    metrics = relationship('Metric', back_populates='user')
-    apartment_id = Column(Integer, ForeignKey('appartments.id'))
+    apartment_id = Column(Integer, ForeignKey('apartments.id'))
     in_location = Column(Boolean)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    metrics = relationship('Metric', back_populates='user')
 
-class Rates(Base):
+class Rates(Base, TimestampMixin):
     __tablename__ = 'rates'
     id = Column(Integer, primary_key=True, autoincrement=True)
     electric_rate = Column(Float)
     water_rate = Column(Float)
     drainage_rate = Column(Float)
-    apartment_id = Column(Integer, ForeignKey('appartments.id'))
-    updated_at = Column(DateTime(timezone=True), server_default=func.now())
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    apartment_id = Column(Integer, ForeignKey('apartments.id'))
 
-class Appartments(Base):
-    __tablename__ = 'appartments'
+class Apartment(Base, TimestampMixin):
+    __tablename__ = 'apartments'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-class Metrics(Base):
+class Metric(Base, TimestampMixin):
     __tablename__ = 'metrics'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     water_usage = Column(Float)
     electric_usage = Column(Float)
     user = relationship('User', back_populates='metrics')
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
