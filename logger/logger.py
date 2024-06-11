a33286cee778b22c99413c7db6e71bc61d50d6c0
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-12 00:41:01 +0300

    add requirements.txt

--- logger/logger.py
+++ logger/logger.py
@@ -1,12 +1,16 @@
 import logging
-from logging.handlers import RotatingFileHandler
-
+import os
 
 def setup_logger():
+    logs_dir = 'logs'
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    handler = RotatingFileHandler('logs/app.log', maxBytes=100000, backupCount=5)
+    log_file = os.path.join(logs_dir, 'app.log')
+    handler = logging.FileHandler(log_file)
     handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
