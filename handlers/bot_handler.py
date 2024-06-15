f1321106ac80ddb6db7e471604dc44e7cb1b91c8
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-15 22:10:43 +0300

    refactor handlers

--- handlers/bot_handler.py
+++ handlers/bot_handler.py
@@ -29,8 +29,7 @@ class BotHandler:
                  router: Router,
                  user_service: UserService,
                  apartment_service: ApartmentService,
-                 metric_service:
-                 MetricService,
+                 metric_service: MetricService,
                  bot: Bot,
                  logger):
         self.router = router
@@ -45,97 +44,97 @@ class BotHandler:
     def setup_handlers(self):
         self.router.message.middleware(self.auth_middleware)
 
-        @self.router.message(Command('start'))
-        async def send_welcome(message: Message):
-            await message.answer("Welcome! Send me your water metrics in the format: 'amount unit', e.g., '200 ml'.")
+        self.router.message.register(self.send_welcome, Command('start'))
+        self.router.message.register(self.help_message, Command('help'))
+        self.router.message.register(self.view_metrics, Command('metrics'))
+        self.router.message.register(self.add_apartment_start, Command('addapartment'))
+        self.router.message.register(self.apartment_name_entered, AddApartment.waiting_for_apartment_name)
+        self.router.message.register(self.apartment_address_entered, AddApartment.waiting_for_apartment_address)
+        self.router.message.register(self.add_metrics_start, Command('addmetrics'))
+        self.router.message.register(self.metrics_entered, AddMetrics.waiting_for_metrics)
+        self.router.message.register(self.add_user_start, Command('adduser'))
+        self.router.message.register(self.user_entered, AddUser.waiting_for_user_name)
+        self.router.message.register(self.handle_admin_response)
 
-        @self.router.message(Command('help'))
-        async def help_message(message: Message):
-            await message.answer(
-                "You can send your water intake metrics in the format: 'amount unit', e.g., '200 ml'.\n"
-                "Commands:\n"
-                "/start - Start the bot\n"
-                "/help - Get help\n"
-                "/metrics - View your water metrics\n"
-                "/addmetrics - Add your water and electric metrics\n"
-                "/addapartment - Add a new apartment")
+    @staticmethod
+    async def send_welcome(message: Message):
+        await message.answer("Welcome! Send me your water metrics in the format: 'amount unit', e.g., '200 ml'.")
 
-        @self.router.message(Command('metrics'))
-        async def view_metrics(message: Message):
-            user_id = message.from_user.id
-            metrics = self.metric_service.get_metrics_for_user(user_id)
-            response = format_metrics(metrics)
-            await message.answer(response)
+    @staticmethod
+    async def help_message(message: Message):
+        await message.answer(
+            "You can send your water intake metrics in the format: 'amount unit', e.g., '200 ml'.\n"
+            "Commands:\n"
+            "/start - Start the bot\n"
+            "/help - Get help\n"
+            "/metrics - View your water metrics\n"
+            "/addmetrics - Add your water and electric metrics\n"
+            "/addapartment - Add a new apartment")
 
-        @self.router.message(Command('addapartment'))
-        async def add_apartment_start(message: Message, state: FSMContext):
-            await message.answer("Please enter the apartment name:")
-            await state.set_state(AddApartment.waiting_for_apartment_name)
+    async def view_metrics(self, message: Message):
+        user_id = message.from_user.id
+        metrics = self.metric_service.get_metrics_for_user(user_id)
+        response = format_metrics(metrics)
+        await message.answer(response)
 
-        @self.router.message(AddApartment.waiting_for_apartment_name)
-        async def apartment_name_entered(message: Message, state: FSMContext):
-            await state.update_data(apartment_name=message.text)
-            await message.answer("Please enter the apartment address:")
-            await state.set_state(AddApartment.waiting_for_apartment_address)
+    @staticmethod
+    async def add_apartment_start(message: Message, state: FSMContext):
+        await message.answer("Please enter the apartment name:")
+        await state.set_state(AddApartment.waiting_for_apartment_name)
 
-        @self.router.message(AddApartment.waiting_for_apartment_address)
-        async def apartment_address_entered(message: Message, state: FSMContext):
-            user_data = await state.get_data()
-            apartment_name = user_data['apartment_name']
-            apartment_address = message.text
-            self.apartment_service.add_apartment(apartment_name, apartment_address)
-            await message.answer(f"Apartment '{apartment_name}' at '{apartment_address}' added successfully.")
+    @staticmethod
+    async def apartment_name_entered(message: Message, state: FSMContext):
+        await state.update_data(apartment_name=message.text)
+        await message.answer("Please enter the apartment address:")
+        await state.set_state(AddApartment.waiting_for_apartment_address)
+
+    async def apartment_address_entered(self, message: Message, state: FSMContext):
+        user_data = await state.get_data()
+        apartment_name = user_data['apartment_name']
+        apartment_address = message.text
+        self.apartment_service.add_apartment(apartment_name, apartment_address)
+        await message.answer(f"Apartment '{apartment_name}' at '{apartment_address}' added successfully.")
+        await state.clear()
+
+    @staticmethod
+    async def add_metrics_start(message: Message, state: FSMContext):
+        await message.answer("Please enter the water and electric metrics in the format: 'water electric', e.g., '200 150'.")
+        await state.set_state(AddMetrics.waiting_for_metrics)
+
+    async def metrics_entered(self, message: Message, state: FSMContext):
+        chat_id = message.from_user.id
+        try:
+            water, electric = map(float, message.text.split())
+            recorded_metric = self.metric_service.add_metrics_for_user(chat_id, water, electric)
+            if recorded_metric:
+                await message.answer(
+                    f"Recorded: Water - {recorded_metric.water_usage}, Electricity - {recorded_metric.electric_usage}")
+            else:
+                await message.answer("Failed to record metrics. Please try again.")
+        except ValueError:
+            await message.answer("Invalid format. Please send in the format: 'water electric', e.g., '200 150'.")
+            return
+        await state.clear()
+
+    @staticmethod
+    async def add_user_start(message: Message, state: FSMContext):
+        await message.answer("Please enter your username:")
+        await state.set_state(AddUser.waiting_for_user_name)
+
+    async def user_entered(self, message: Message, state: FSMContext):
+        chat_id, user_name = message.text.split(" ")
+        try:
+            user = self.user_service.add_user(chat_id, user_name, first_name=user_name, apartment_id=1, in_location=True)
+            if user:
+                await message.answer(f"User {user_name} added successfully.")
+            else:
+                await message.answer("Failed to add user. Please try again.")
+        except Exception as ex:
+            error_message = "Failed to add user. Please try again."
+            self.logger.error(f"Error adding user: {ex}")
+            await message.answer(error_message)
+        finally:
             await state.clear()
 
-        @self.router.message(Command('addmetrics'))
-        async def add_metrics_start(message: Message, state: FSMContext):
-            await message.answer(
-                "Please enter the water and electric metrics in the format: 'water electric', e.g., '200 150'.")
-            await state.set_state(AddMetrics.waiting_for_metrics)
-
-        @self.router.message(AddMetrics.waiting_for_metrics)
-        async def metrics_entered(message: Message, state: FSMContext):
-            chat_id = message.from_user.id
-            try:
-                water, electric = map(float, message.text.split())
-                recorded_metric = self.metric_service.add_metrics_for_user(chat_id, water, electric)
-                if recorded_metric:
-                    await message.answer(
-                        f"Recorded: Water - {recorded_metric.water_usage}, "
-                        f"Electricity - {recorded_metric.electric_usage}")
-                else:
-                    await message.answer("Failed to record metrics. Please try again.")
-            except ValueError:
-                await message.answer("Invalid format. Please send in the format: 'water electric', e.g., '200 150'.")
-                return
-            await state.clear()
-
-        @self.router.message(Command('adduser'))
-        async def add_user_start(message: Message, state: FSMContext):
-            await message.answer("Please enter your username:")
-            await state.set_state(AddUser.waiting_for_user_name)
-
-        @self.router.message(AddUser.waiting_for_user_name)
-        async def user_entered(message: Message, state: FSMContext):
-            chat_id, user_name = message.text.split(" ")
-            try:
-                user = self.user_service.add_user(chat_id,
-                                                  user_name,
-                                                  first_name=user_name,
-                                                  apartment_id=1,
-                                                  in_location=True
-                                                  )
-                if user:
-                    await message.answer(f"User {user_name} added successfully.")
-                else:
-                    await message.answer("Failed to add user. Please try again.")
-            except Exception as ex:
-                error_message = "Failed to add user. Please try again."
-                self.logger.error(f"Error adding user: {ex}")
-                await message.answer(error_message)
-            finally:
-                await state.clear()
-
-        @self.router.message()
-        async def handle_admin_response(message: Message):
-            await self.auth_middleware.process_approval(message)
+    async def handle_admin_response(self, message: Message):
+        await self.auth_middleware.process_approval(message)
