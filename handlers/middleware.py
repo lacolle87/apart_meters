c997526490754377cb023f821257b64ac64d5140
Nikita Kuznetsov <Lacolle87@gmail.com>
2024-06-15 22:05:01 +0300

    approve works

--- handlers/middleware.py
+++ handlers/middleware.py
@@ -21,9 +21,8 @@ class AuthMiddleware(BaseMiddleware):
                 await event.answer("Your registration is pending approval. Please wait for the superuser to confirm.")
                 return
 
-            await event.answer("You need to be authenticated to use this bot. Please register first.")
             await self.bot.send_message(
-                admins[0],
+                admins[0].chat_id,
                 f"New user {event.from_user.username} ({user_id}) is trying to access the bot. Approve? Reply with 'yes' or 'no'."
             )
             self.pending_approvals[user_id] = event.from_user.username
@@ -34,7 +33,7 @@ class AuthMiddleware(BaseMiddleware):
     async def process_approval(self, message: Message):
         admin_id = message.from_user.id
         admins = self.user_service.get_admins()
-        if admin_id != admins[0]:
+        if admin_id != admins[0].chat_id:
             await message.answer("You are not authorized to approve users.")
             return
 
@@ -47,24 +46,20 @@ class AuthMiddleware(BaseMiddleware):
             await message.answer("Please reply with 'yes' or 'no'.")
 
     async def handle_approval(self, message: Message, approved: bool):
-        pending_approvals = self.pending_approvals
-        if not pending_approvals:
+        if not self.pending_approvals:
             await message.answer("There are no pending user approvals.")
             return
 
-        user_id, username = pending_approvals.popitem()
+        user_id, username = next(iter(self.pending_approvals.items()))
         if approved:
-            await self.approve_user(user_id)
-            await message.answer(f"User {username} ({user_id}) has been approved.")
+            await self.approve_user(user_id, username)
         else:
             await self.deny_user(user_id)
-            await message.answer(f"User {username} ({user_id}) has been denied.")
 
-    async def approve_user(self, user_id: int):
-        self.user_service.add_user(user_id, self.pending_approvals[user_id], self.pending_approvals[user_id], 0, False)
-        if user_id in self.pending_approvals:
-            del self.pending_approvals[user_id]
-            await self.bot.send_message(user_id, "Your registration has been approved. You can now use the bot.")
+    async def approve_user(self, user_id: int, username: str):
+        self.user_service.add_user(user_id, username, username, 0, False)
+        await self.bot.send_message(user_id, "Your registration has been approved. You can now use the bot.")
+        del self.pending_approvals[user_id]
 
     async def deny_user(self, user_id: int):
         if user_id in self.pending_approvals:
